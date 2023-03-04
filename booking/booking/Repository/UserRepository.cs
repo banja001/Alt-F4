a04994a1eff5c578dbea8c65701cf3ce2d827c62
Nevena Gligorov <gligorov.nevena@gmail.com>
2023-03-04 18:12:47 +0100

    inital project version

---
+++ booking/booking/Repository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using booking.Model;
+using booking.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booking.Repository
+{
+    public class UserRepository
+    {
+        private const string FilePath = "../../../Resources/Data/users.csv";
+
+        private readonly Serializer<User> _serializer;
+
+        private List<User> _users;
+
+        public UserRepository()
+        {
+            _serializer = new Serializer<User>();
+            _users = _serializer.FromCSV(FilePath);
+        }
+
+        public User GetByUsername(string userName)
+        {
+            _users = _serializer.FromCSV(FilePath);
+            return _users.FirstOrDefault(u => u.Username == userName);
+        }
+    }
+}
---
+++ booking/booking/View/SignInForm.xaml.cs
@@ -0,0 +1,93 @@
+﻿using booking.Model;
+using booking.Repository;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace booking.View
+{
+    /// <summary>
+    /// Interaction logic for SignInForm.xaml
+    /// </summary>
+    public partial class SignInForm : Window
+    {
+
+        private readonly UserRepository _repository;
+
+        private string _userName;
+
+        public string UserName
+        {
+            get => _userName;
+            set
+            {
+                if (value != _userName)
+                {
+                    _userName = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public SignInForm()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _repository = new UserRepository();
+        }
+
+        private void SignIn(object sender, RoutedEventArgs e)
+        {
+            User user = _repository.GetByUsername(UserName);
+
+            if(user != null)
+            {
+                if(user.Password == txtPassword.Password)
+                {
+                    if (user.Role == "Owner")
+                    {
+
+                    }
+                    else if (user.Role == "Guest1")
+                    {
+
+                    }
+                    else if(user.Role == "Guest2")
+                    {
+                        
+                    } 
+                    else if(user.Role == "Guide")
+                    {
+
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Wrong passwrod!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Wrong username!");
+            }
+        }
+    }
+}
