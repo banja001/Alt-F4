f1923206f92157861241794450961a2a9b0ac0dd
Banja001 <114954110+banja001@users.noreply.github.com>
2023-03-06 10:12:45 +0100

    Class Tour is created

---
+++ booking/booking/Model/CheckPoint.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booking.Model
+{
+    internal class CheckPoint
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        bool Active { get; set; }
+        int TourId { get; set; }
+        int Order { get; set; }
+
+        public CheckPoint() 
+        {
+
+        }
+
+        public CheckPoint(int id, string name, bool active, int tourId, int order)
+        {
+            Id = id;
+            Name = name;
+            Active = active;
+            TourId = tourId;
+            Order = order;
+        }
+    }
+}
---
+++ booking/booking/Model/Tour.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booking.Model
+{
+    internal class Tour
+    {
+        int Id {  get; set; }
+        string Name { get; set; };
+        string Location { get; set; }
+        string Description { get; set; }
+        string Language { get; set; }
+        int MaxGuests { get; set; }
+        DateTime StartTime { get; set; }
+        double Duration { get; set; }
+
+        public Tour()
+        { }
+
+        public Tour(int id, string name, string description, string language, int maxGuests,DateTime startTime, double duration)
+        {
+            Id = id;
+            Name = name;   
+            Description = description;
+            Language = language;
+            MaxGuests = maxGuests;
+            StartTime = startTime;
+            Duration = duration;
+        }
+    }
+}
