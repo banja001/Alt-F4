f9a3587249d1d5c9df5efd0f974d26f933aafd32
JovanKatanic <jovan.katanic0101@gmail.com>
2023-04-17 22:55:17 +0200

    AddAccommodationimageViewModel

---
+++ booking/booking/WPF/ViewModels/Owner/AddAccommodationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF.ViewModels.Owner
+{
+    public class AddAccommodationViewModel
+    {
+
+    }
+}
--- booking/booking/WPF/Views/Owner/AddAccommodationImageWindow.xaml.cs
+++ booking/booking/WPF/Views/Owner/AddAccommodationImageWindow.xaml.cs
@@ -3,6 +3,7 @@ using booking.Repository;
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
+using WPF.ViewModels.Owner;
 
 namespace booking.View.Owner
 {
@@ -11,32 +12,15 @@ namespace booking.View.Owner
     /// </summary>
     public partial class AddAccommodationImageWindow : Window
     {
-        
-        private List<string> accommodationImages;
-        
+
         public AddAccommodationImageWindow(List<string> images)
         {
             InitializeComponent();
-            DataContext = this;
-            accommodationImages = images;  
+            DataContext = new AddAccommodationImageViewModel(images);
+             
 
         }
 
-        public void ConfirmImageClick(object sender, RoutedEventArgs e)
-        {
-            if (string.IsNullOrEmpty(UrlTextBox.Text))
-            {
-                MessageBox.Show("Please fill all of the textboxes");
-                return;
-            } 
-            accommodationImages.Add(UrlTextBox.Text);
-            this.Close();
-
-        }
-
-        public void CancelImageClick(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
+        
     }
 }
