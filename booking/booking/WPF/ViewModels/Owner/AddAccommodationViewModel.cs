c9296b7fb62a549c1fe73b93805e65e4d3657a96
JovanKatanic <jovan.katanic0101@gmail.com>
2023-05-03 02:58:10 +0200

    Add acc slike

--- booking/booking/WPF/ViewModels/Owner/AddAccommodationViewModel.cs
+++ booking/booking/WPF/ViewModels/Owner/AddAccommodationViewModel.cs
@@ -23,13 +23,46 @@ using booking.WPF.ViewModels;
 
 namespace WPF.ViewModels.Owner
 {
-    public class AddAccommodationViewModel : BaseViewModel
+    public class AddAccommodationViewModel : BaseViewModel,INotifyPropertyChanged
     {
         
         private List<string> accommodationImagesUrl;
         public OwnerViewModel ownerViewModel;
         //public List<string> StateList;
 
+        private bool nextEnabled;
+        public bool NextEnabled
+        {
+            get
+            {
+                return nextEnabled;
+            }
+            set
+            {
+                if (value != nextEnabled)
+                {
+                    nextEnabled = value;
+                    OnPropertyChanged("NextEnabled");
+                }
+            }
+        }
+
+        private bool prevEnabled;
+        public bool PrevEnabled
+        {
+            get
+            {
+                return prevEnabled;
+            }
+            set
+            {
+                if (value != prevEnabled)
+                {
+                    prevEnabled = value;
+                    OnPropertyChanged("PrevEnabled");
+                }
+            }
+        }
         public string State { get; set; }
         public string City { get; set; }
         public string Name { get; set; }
@@ -39,12 +72,44 @@ namespace WPF.ViewModels.Owner
         public string DaysToCancel { get; set; }
 
         private string image;
-        public string ImageUrl {get;set;}
+        public string ImageUrl {
+            get
+            {
+                return image;
+            }
+            set
+            {
+                if (value != image)
+                {
+                    image = value;
+                    OnPropertyChanged("ImageUrl");
+                }
+            }
+            }
 
         public string SelectedItemCity { get; set; }
         public string SelectedItemState { get; set; }
-        
+
+        private BitmapImage slika;
+        public BitmapImage Slika { 
+            get
+            {
+                return slika;
+            }
+            set
+            {
+                if (value != slika)
+                {
+                    slika = value;
+                    OnPropertyChanged("Slika");
+                }
+                
+            }
+            }
+        public int ActiveImageIndx { get; set; }
         public List<string> StateList { get; set; }
+        public ICommand NextPictureCommand => new RelayCommand(NextPictureClick);
+        public ICommand PrevPictureCommand => new RelayCommand(PrevImageButtonClick);
         public ICommand ConfirmCommand => new RelayCommand(Confirm);
         public ICommand AddImageCommand => new RelayCommand(AddImageClick);
         //public ICommand ComboboxSelectionChangedCommand => new RelayCommand(StateComboBox_SelectionChanged);
@@ -139,7 +204,7 @@ namespace WPF.ViewModels.Owner
                 accommodationImagesUrl.Add(ImageUrl);
                 ImageUrl = "";
             }
-            
+            ShowImage();
 
         }
         /*
@@ -158,7 +223,63 @@ namespace WPF.ViewModels.Owner
                 accommodationImagesUrl.RemoveAt(accommodationImagesUrl.Count() - 1);
                 MessageBox.Show("Image removed", "Message");
             }
+            ShowImage();
+        }
 
+        public void SetImageSource(string url)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(@url, UriKind.Absolute);
+            bitmapImage.EndInit();
+            Slika = bitmapImage;
+        }
+        public void ShowImage()
+        {
+            CheckIndexScope();
+            if (accommodationImagesUrl.Count == 0)
+            {
+                Slika = null;
+               
+                return;
+            }
+
+            ActiveImageIndx = 0;
+            SetImageSource(accommodationImagesUrl[ActiveImageIndx]);
+            CheckIndexScope();
+        }
+
+        public void NextPictureClick()
+        {
+            SetImageSource(accommodationImagesUrl[++ActiveImageIndx]);
+            CheckIndexScope();
+        }
+
+        private void PrevImageButtonClick()
+        {
+            SetImageSource(accommodationImagesUrl[--ActiveImageIndx]);
+            CheckIndexScope();
+        }
+        
+        public void CheckIndexScope()
+        {
+            if (ActiveImageIndx + 1 >= accommodationImagesUrl.Count)
+            {
+                NextEnabled = false;
+            }
+            else
+            {
+                NextEnabled = true;
+            }
+
+            if (ActiveImageIndx <= 0)
+            {
+                PrevEnabled = false;
+            }
+            else
+            {
+                PrevEnabled = true;
+            }
         }
     }
 }
