a56d079534563332215de98579d57d1ca2914360
JovanKatanic <jovan.katanic0101@gmail.com>
2023-05-16 18:42:23 +0200

    Added guest1rating ReservationId

--- booking/booking/WPF/ViewModels/Owner/OwnerViewModel.cs
+++ booking/booking/WPF/ViewModels/Owner/OwnerViewModel.cs
@@ -158,6 +158,7 @@ namespace WPF.ViewModels.Owner
                 guestsToRate.EndDate = date.EndDate.ToShortDateString();
                 guestsToRate.GuestName = users.Find(u => u.Id == date.UserId).Username;
                 guestsToRate.AccommodationName = accommodations.Find(u => u.Id == date.AccommodationId).Name;
+                //guestsToRate.ReservationId=date.
                 tempList.Add(guestsToRate);
             }
             return tempList;
