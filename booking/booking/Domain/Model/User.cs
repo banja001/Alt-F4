2fa8a387d86e12a71cc0ab2623551ed118f03770
Nevena Gligorov <gligorov.nevena@gmail.com>
2023-05-18 01:03:05 +0200

    feat: updated User class for superGuest1

--- booking/booking/Domain/Model/User.cs
+++ booking/booking/Domain/Model/User.cs
@@ -1,6 +1,7 @@
 ﻿using booking.Serializer;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,23 +17,31 @@ namespace booking.Model
         public string Role { get; set; }
 
         public bool Super { get; set; }
+        public DateTime DateOfBecomingSuper { get; set; }
         public int Score { get; set; }
+        public int NumOfAccommodationReservations { get; set; }
         public User() 
         {
         }
 
-        public User(string username, string password, string role, bool super=false, int score = 0)
+        public User(string username, string password, string role, bool super=false, 
+            DateTime? dateOfBecomingSuper = null, int score = 0, int numOfAccommodationReservations = 0)
         {
             Username = username;
             Password = password;
             Role = role;
             Super = super;
             Score = score;
+            if (!dateOfBecomingSuper.HasValue)
+                dateOfBecomingSuper = DateTime.Now;
+            DateOfBecomingSuper = dateOfBecomingSuper.Value;
+            NumOfAccommodationReservations = numOfAccommodationReservations;
         }
 
         public string[] ToCSV()
         {
-            string[] csvValues = { Id.ToString(), Username, Password, Role,Super.ToString(), Score.ToString() };
+            string[] csvValues = { Id.ToString(), Username, Password, Role ,Super.ToString(),
+                DateOfBecomingSuper.ToString("dd/MM/yyyy"), Score.ToString(), NumOfAccommodationReservations.ToString() };
             return csvValues;
         }
 
@@ -43,7 +52,9 @@ namespace booking.Model
             Password = values[2];
             Role = values[3];
             Super = Convert.ToBoolean(values[4]);
-            Score = Convert.ToInt32(values[5]);
+            DateOfBecomingSuper = DateTime.ParseExact(values[5], "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Score = Convert.ToInt32(values[6]);
+            NumOfAccommodationReservations = Convert.ToInt32(values[7]);
         }
 
         public string IsSuper()
