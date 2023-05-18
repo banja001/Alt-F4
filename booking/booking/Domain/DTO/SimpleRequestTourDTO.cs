b6fc380715e9a6a7d32c485d7f2249d48afe2eb0
Marko Radetic <markoradetic67@gmail.com>
2023-05-18 21:56:53 +0200

    Feat: Added notifications methods.

---
+++ booking/booking/Domain/DTO/SimpleRequestTourDTO.cs
@@ -0,0 +1,29 @@
+﻿using booking.Model;
+using Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.DTO
+{
+    public class SimpleRequestTourDTO
+    {
+        public int Id { get; set; } 
+        public Tour Tour { get; set; }
+        public SimpleRequest SimpleRequest { get; set; }
+        public string NotificationDescription { get; set; }
+
+        public SimpleRequestTourDTO() 
+        {
+            Tour = new Tour();
+            SimpleRequest = new SimpleRequest();    
+        }
+        public SimpleRequestTourDTO(int id, Tour tour, SimpleRequest simpleRequest)
+        {
+            Id = id;
+            Tour = tour;
+            SimpleRequest = simpleRequest;
+
+        }
+    }
+}
---
+++ booking/booking/WPF/ViewModels/HomeViewModel.cs
@@ -0,0 +1,24 @@
+﻿using application.UseCases;
+using booking.Model;
+using booking.WPF.ViewModels;
+using Domain.DTO;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace booking.WPF.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public ObservableCollection<SimpleRequestTourDTO> SuggestionNotifications { get; set; }
+        public ObservableCollection<SimpleRequestTourDTO> ApprovedNotifications { get; set; }
+        private SimpleRequestService _simpleRequestService;
+        public HomeViewModel(User user)
+        {
+            _simpleRequestService = new SimpleRequestService();
+            SuggestionNotifications = new ObservableCollection<SimpleRequestTourDTO>(_simpleRequestService.CreateSuggestionNotificationsByGuest2(user));
+            ApprovedNotifications = new ObservableCollection<SimpleRequestTourDTO>(_simpleRequestService.CreateApprovedNotificationsByGuest2(user));
+        }
+    }
+}
